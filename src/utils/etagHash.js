a946c58733398389d9b9ab119a4cc2d4a07722a4
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2025-02-08 10:57:11 -0500

    Revert "first commit"
    This reverts commit 08bf025da1ad24a997ad65c1c81bae09dbf037b6.

--- src/utils/etagHash.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const etagCreater = (data) => {
-  // Create an MD5 hash of the data
-  return crypto.createHash('md5').update(data).digest('hex');
-};
-
-module.exports = { etagCreater };
--- src/utils/validate.js
+++
@@ -1,7 +0,0 @@
-const Ajv = require('ajv');
-const planSchema = require('../schema/plan.schema.json');
-
-const ajv = new Ajv();
-const validate = ajv.compile(planSchema);
-
-module.exports = {validate};
